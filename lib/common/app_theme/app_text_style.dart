b6e8f8f12f5273bd2a03ebda32a4aef59ca95794
iqthuc <thucnq@qsoft.vn>
2023-10-16 15:35:51 +0700

    config dynamic theme, text style

---
+++ lib/common/app_theme/app_text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  final TextTheme _appTextTheme = const TextTheme(
+      labelLarge: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 0.25,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 0.25,
+      ));
+
+  TextStyle get myTextSTyle => _appTextTheme.bodyLarge!;
+  TextStyle get myTextSTyle2 => _appTextTheme.labelLarge!;
+}
--- lib/common/app_theme/index.dart
+++ lib/common/app_theme/index.dart
@@ -1,4 +1,2 @@
-export 'app_colors.dart';
-export 'app_styles.dart';
+export 'app_text_style.dart';
 export 'app_theme.dart';
-export 'app_themes.dart';
